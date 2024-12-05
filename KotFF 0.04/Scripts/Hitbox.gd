169240daa52bb70e8252a2c396755e9fcb130c5d
HunterSlimeG <139635683+HunterSlimeG@users.noreply.github.com>
2024-12-05 16:11:07 -0500

    Delete KotFF 0.04 directory

--- KotFF 0.04/Scripts/Hitbox.gd
+++
@@ -1,9 +0,0 @@
-class_name Hitbox
-extends Area2D
-
-@export var value := 5
-@export_enum("Damage:1", "Mana:2", "Health:3") var type : int = 1
-func _init():
-	monitoring = false
-	collision_layer = 2
-	collision_mask = 0
