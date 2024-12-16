be0189b1d0c9074e8d72e8c09b4b24b1db6f1606
HunterSlimeG <139635683+HunterSlimeG@users.noreply.github.com>
2024-12-16 07:43:25 -0500

    Add files via upload

---
+++ KotFF 0.15/Scripts/Interaction.gd
@@ -0,0 +1,39 @@
+extends Control
+
+@export_enum("Off:0","Teleporter:1", "Inventory:2", "Dialogue:3", "Storage:4") var mode : int = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_back_pressed() -> void:
+	visible = false
+	Global.menu = false
+	$Teleporter.visible = false
+	$Inventory.visible = false
+	#$Dialogue.visible = false
+	#$Storage.visible = false
+func active(type: int):
+	Global.menu = true
+	mode = type
+	visible = true
+	if mode == 0:
+		Global.menu = false
+		visible = false
+		$Teleporter.visible = false
+		$Inventory.visible = false
+		#$Dialogue.visible = false
+		#$Storage.visible = false
+	elif mode == 1:
+		$Teleporter.visible = true
+	elif mode == 2:
+		$Inventory.visible = true
+	elif mode == 3:
+		$Dialogue.visible = true
+	elif mode == 4:
+		$Storage.visible = true
